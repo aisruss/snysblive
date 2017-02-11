7f82efb1959956f30b88e96ea3c8976907b33171
aislinnr <ais_russell@hotmail.com>
2017-02-11 09:09:38 -0600

    adding all files

---
+++ media.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : OfficialWork 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20121012
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>South Norfolk Youth Symphonic Band +</title>
+<link href="http://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+<style type="text/css">
+@import "gallery.css";
+</style>
+</head>
+<body>
+<div id="wrapper">
+    <div id="uberbar">
+<img src="images/logo.png" width="110" height="110" alt="" />
+
+</div>
+    <div id="banner"><img src="images/bandfade.jpg"  width="550" height="300" alt="" style="margin-left: 120px;border-radius: 20px;" /></div>
+	<div id="header">
+		<div id="logo">
+			<h1><a href="#">South Norfolk Youth Symphonic Band +</a></h1>
+		</div>
+	</div>
+	<!-- end #header -->
+	<div id="menu">
+		<ul>
+			<li><a href="index.html">Home</a></li>
+			<li><a href="concerts.html">Concerts</a></li>
+			<li><a href="joining.html">Join SNYSB</a></li>
+			<li><a href="contact.html">Contact</a></li>
+			<li><a href="guestbook.html">Guestbook</a></li>
+			<!--<li><a href="Gallery.php">Photos</a></li>-->
+			<li class="current_page_item"><a href="media.php">Media</a></li>
+		</ul>
+	</div>
+	
+	<div id="welcome">
+		<!--<h2 class="title"><a href="#">South Norfolk Youth Symphonic Band +</a></h2>-->
+		<div class="entry">
+         <div class="title"><h2>Media</h2></div>
+			Here is a selection of music we play, recorded from previous concerts! <br><br>
+                    <!--
+                    BEGIN JAVASCRIPT PLAYER EMBED CODE 
+                    More players and documentation at: www.premiumbeat.com/flash_media_players
+                    -->
+                    
+                    <center><script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script>
+                    <div id="media-player"></div>
+                    
+                    <script type="text/javascript">
+                    var options = {};
+                    options.fontSize = "large";
+                    options.playlistXmlPath = "playlist.xml";
+                    options.autoPlay = "true";
+                    options.shufflePlaylist = "true";
+                    options.defaultVolume = "75";
+                    
+                    var params = {};
+                    params.allowScriptAccess = "always";
+                    
+                    swfobject.embedSWF("OriginalMusicPlayerPlaylist.swf", "media-player", "360", "400", "9.0.0","expressInstall.swf", options, params);
+                    </script></center>
+                    <!-- END PLAYER EMBED CODE -->
+
+                     <h2 class="heading">Photo Gallery</h2><br>
+            <a href="gallery.html">Click here to view photos</a>
+                    
+                    <h2 class="heading">Video</h2><br>
+					Here is a selection of videos recorded of the bands 
+					performances!<br>
+					<br>
+					Proms in the Park 2011:<br>
+
+					<strong>Gaelforce</strong><br>
+					<center><iframe width="640" height="390" src="http://www.youtube.com/embed/OwniC4707wo?hd=1" frameborder="0" allowfullscreen></iframe></center><br><br>
+					<strong>Pirates Of The Caribbean</strong><br>
+					<center><iframe width="640" height="390" src="http://www.youtube.com/embed/6POsyGp-7QY?hd=1" frameborder="0" allowfullscreen></iframe></center><br><br>
+					<strong>Latin Gold</strong><br>
+					<center><iframe width="640" height="390" src="http://www.youtube.com/embed/rcDEALhx3jE?hd=1" frameborder="0" allowfullscreen></iframe></center><br><br>
+                        
+		</div>
+	</div>
+
+	<!-- end #page --> 
+</div>
+<div id="footer">
+	<p>&copy; South Norfolk Youth Symphonic Band +</p>
+</div>
+<!-- end #footer -->
+</body>
+</html>
